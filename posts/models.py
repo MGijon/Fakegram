64a71b0f801c79a62819890e93be4eb7d37f3b33
Unknown <mgijon94@gmail.com>
2019-08-27 20:22:05 +0200

    Changes in the User models created with instructional prupose

--- posts/models.py
+++ posts/models.py
@@ -11,11 +11,74 @@ class User(models.Model):
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
 
+	country = models.CharField(max_length=100, default=False)
+	city = models.CharField(max_length=100, default=False)
+
 	is_admin = models.BooleanField(default = False)
 
-	bio = models.TextField()
+	bio = models.TextField(blank=True)   
 
 	birthdate = models.DateField(blank=True, null=True) # we allow non added and the deefault value is now
 
 	created = models.DateTimeField(auto_now_add=True) # filled when created
 	modified = models.DateTimeField(auto_now=True)  # filled when opened  
+
+''' Fake data generated with instructional purpose:
+users = [
+	{
+		email = 'andromeda@gmail.com',
+		password = '1234567',
+		first_name = 'andromeda',
+		last_name = 'gonzales',
+		city = 'Barcelona',
+		country = 'Spain',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+	{
+		email = '',
+		password = '',
+		first_name = '',
+		last_name = '',
+	},
+]
