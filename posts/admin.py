7052cf861712aa49f39f30c97f5747ba1505e46d
Unknown <mgijon94@gmail.com>
2019-09-06 18:50:40 +0200

    model Posts added to the admins page

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,24 @@
+"""Posts admin classes."""
+
+# Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
+# Models 
+from django.contrib.auth.models import User
+from posts.models import Posts
+
 # Register your models here.
+
+@admin.register(Posts)
+class ProfileAdmin(admin.ModelAdmin):
+	"""Posts admin."""
+
+
+
+class ProfileInLine(admin.StackedInline):
+	"""Profile in-line admin for users."""
+
+	model = Posts
+	can_delete = False
+	verbose_name_plural = 'profiles'
